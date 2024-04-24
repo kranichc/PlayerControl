9c37ceaaa795cf03ef8825ab8fa1f1c6e5139491
Charles Kranich <kranichc@roverkids.org>
2024-04-24 14:16:14 -0400

    finished project

--- PlayerControl/Assets/Scripts/PlayerControl.cs
+++ PlayerControl/Assets/Scripts/PlayerControl.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class PlayerControl : MonoBehaviour
 {
+    // Private VARIABLES
     public float speed = 5.0f;
     public float turnSpeed;
     public float horizontalInput;
@@ -18,11 +19,12 @@ public class PlayerControl : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // this is where we get player input
         horizontalInput = Input.GetAxis("Horizontal");
         forwardInput = Input.GetAxis("Vertical");
-
         // We'll move the vehicle forward
         transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
-        transform.Rotate(Vector3.up, Time.deltaTime * turnSpeed * horizontalInput);
+        // we turn the vehicle
+        transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime);
     }
 }
